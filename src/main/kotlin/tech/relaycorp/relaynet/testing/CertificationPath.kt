c1576cfaa54f637ce9ec99eacb09efecfbf25b62
Gus Narea <gnarea@users.noreply.github.com>
2020-09-08 23:25:29 +0100

    feat: Implement dummy keys

---
+++ src/main/kotlin/tech/relaycorp/relaynet/testing/CertificationPath.kt
@@ -0,0 +1,52 @@
+package tech.relaycorp.relaynet.testing
+
+import tech.relaycorp.relaynet.issueEndpointCertificate
+import tech.relaycorp.relaynet.issueGatewayCertificate
+import tech.relaycorp.relaynet.issueParcelDeliveryAuthorization
+import tech.relaycorp.relaynet.wrappers.x509.Certificate
+import java.time.ZonedDateTime
+
+@Suppress("MemberVisibilityCanBePrivate")
+public object CertificationPath {
+    private val startDate = ZonedDateTime.now().minusMinutes(1)
+    private val endDate = ZonedDateTime.now().plusHours(1)
+
+    public val PUBLIC_GW: Certificate by lazy {
+        issueGatewayCertificate(
+            KeyPairSet.PUBLIC_GW.public,
+            KeyPairSet.PUBLIC_GW.private,
+            endDate,
+            validityStartDate = startDate
+        )
+    }
+
+    public val PRIVATE_GW: Certificate by lazy {
+        issueGatewayCertificate(
+            KeyPairSet.PRIVATE_GW.public,
+            KeyPairSet.PUBLIC_GW.private,
+            endDate,
+            PUBLIC_GW,
+            validityStartDate = startDate
+        )
+    }
+
+    public val PRIVATE_ENDPOINT: Certificate by lazy {
+        issueEndpointCertificate(
+            KeyPairSet.PRIVATE_ENDPOINT.public,
+            KeyPairSet.PRIVATE_GW.private,
+            endDate,
+            PRIVATE_GW,
+            validityStartDate = startDate
+        )
+    }
+
+    public val PDA: Certificate by lazy {
+        issueParcelDeliveryAuthorization(
+            KeyPairSet.PDA_GRANTEE.public,
+            KeyPairSet.PRIVATE_ENDPOINT.private,
+            endDate,
+            PRIVATE_ENDPOINT,
+            validityStartDate = startDate
+        )
+    }
+}
