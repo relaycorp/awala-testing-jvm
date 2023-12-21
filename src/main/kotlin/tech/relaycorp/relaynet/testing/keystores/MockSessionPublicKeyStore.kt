a0cc202cf00cbb619268fd2b3fe5eaf7358ea581
Gus Narea <gnarea@users.noreply.github.com>
2023-12-21 10:04:39 +0000

    fix(deps): Upgrade to Awala v1.68.5 (#134)

--- src/main/kotlin/tech/relaycorp/relaynet/testing/keystores/MockSessionPublicKeyStore.kt
+++ src/main/kotlin/tech/relaycorp/relaynet/testing/keystores/MockSessionPublicKeyStore.kt
@@ -8,28 +8,33 @@ public class MockSessionPublicKeyStore(
     private val retrievalException: Throwable? = null,
 ) : SessionPublicKeyStore() {
     @Suppress("MemberVisibilityCanBePrivate")
-    public val keys: MutableMap<String, SessionPublicKeyData> = mutableMapOf()
+    public val keys: MutableMap<String, MutableMap<String, SessionPublicKeyData>> = mutableMapOf()
 
     public fun clear() {
         keys.clear()
     }
 
-    override suspend fun saveKeyData(keyData: SessionPublicKeyData, peerId: String) {
+    override suspend fun saveKeyData(
+        keyData: SessionPublicKeyData,
+        nodeId: String,
+        peerId: String
+    ) {
         if (savingException != null) {
             throw savingException
         }
-        this.keys[peerId] = keyData
+        this.keys.putIfAbsent(nodeId, mutableMapOf())
+        this.keys[nodeId]!![peerId] = keyData
     }
 
-    override suspend fun retrieveKeyData(peerId: String): SessionPublicKeyData? {
+    override suspend fun retrieveKeyData(nodeId: String, peerId: String): SessionPublicKeyData? {
         if (retrievalException != null) {
             throw retrievalException
         }
 
-        return keys[peerId]
+        return keys[nodeId]?.get(peerId)
     }
 
-    override suspend fun delete(peerId: String) {
-        keys.remove(peerId)
+    override suspend fun delete(nodeId: String, peerId: String) {
+        keys[nodeId]?.remove(peerId)
     }
 }
